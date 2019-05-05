21a08c7ded6487066b7921495ecdc8f7a6546f15
sohyeon <sohyun1018@gmail.com>
2019-05-05 20:32:28 +0900

    project init

---
+++ Final_Project 3/Pinterest/Views/SecondCell.swift
@@ -0,0 +1,15 @@
+//
+//  SecondCell.swift
+//  Pinterest
+//
+//  Created by 박소현 on 05/05/2019.
+//  Copyright © 2019 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondCell: UICollectionViewCell {
+  
+  @IBOutlet var imgView: UIImageView!
+  
+}
