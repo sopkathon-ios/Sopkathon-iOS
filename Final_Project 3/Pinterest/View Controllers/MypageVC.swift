cb67fd8bd0334dd125340b3054253debee494e80
sohyeon <sohyun1018@gmail.com>
2019-05-05 21:05:31 +0900

    마이페이지VC만듬

---
+++ Final_Project 3/Pinterest/View Controllers/MypageVC.swift
@@ -0,0 +1,38 @@
+//
+//  MypageVC.swift
+//  Pinterest
+//
+//  Created by 박소현 on 05/05/2019.
+//  Copyright © 2019 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class MypageVC: UIViewController {
+
+    
+    @IBOutlet var imgBtn: UIButton!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var myMemoriesLabel: UILabel!
+    @IBOutlet var ourMemoriesLabel: UILabel!
+    @IBOutlet var myMemoriesBtn: UIButton!
+    @IBOutlet var ourMemoriesBtn: UIButton!
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+    
+    @IBAction func clickImgBtn(_ sender: Any) {
+    }
+    
+    @IBAction func clickMyMemoriesAction(_ sender: Any) {
+    }
+    
+    @IBAction func clickOurMemoriesAction(_ sender: Any) {
+    }
+}
