54bd93e25bf62a65b0565ab79554cb1a26b935b5
sohyeon <sohyun1018@gmail.com>
2019-05-05 22:02:34 +0900

    [추가] 마이페이지뷰

--- Final_Project 3/Pinterest/View Controllers/MypageVC.swift
+++ Final_Project 3/Pinterest/View Controllers/MypageVC.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MypageVC: UIViewController {
+    
+    var categories = ["Action", "Drama", "Science Fiction", "Kids", "Horror"]
 
     
     @IBOutlet var imgBtn: UIButton!
@@ -34,5 +36,27 @@ class MypageVC: UIViewController {
     }
     
     @IBAction func clickOurMemoriesAction(_ sender: Any) {
+    }
+}
+
+extension MypageVC : UITableViewDelegate {}
+
+extension MypageVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return categories[section]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryRow") as! CategoryRow
+        return cell
     }
 }
